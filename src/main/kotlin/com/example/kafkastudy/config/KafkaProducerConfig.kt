e40774155b36a518f8f899cabd3c1ee10d6af160
yjna2316 <yjna2316@gmail.com>
2023-05-16 15:15:41 +0900

    feat: KafkaProducer, Confiig 추가

---
+++ src/main/kotlin/com/example/kafkastudy/config/KafkaProducerConfig.kt
@@ -0,0 +1,26 @@
+package com.example.kafkastudy.config
+
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+
+@Configuration
+class KafkaProducerConfig {
+    @Bean
+    fun producerFactory(): ProducerFactory<String, String> {
+        val configProps: MutableMap<String, Any> = HashMap()
+        configProps[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:29092,localhost:39093,localhost:49092"
+        configProps[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configProps[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, String> {
+        return KafkaTemplate(producerFactory())
+    }
+}
---
+++ src/main/kotlin/com/example/kafkastudy/producer/KafkaProducer.kt
@@ -0,0 +1,33 @@
+package com.example.kafkastudy.producer
+
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import org.springframework.stereotype.Component
+import java.util.concurrent.CompletableFuture
+
+@Component
+class KafkaProducer(
+    val kafkaTemplate: KafkaTemplate<String, String>
+) {
+    fun sendMessage(topic: String, message: String) {
+        val future: CompletableFuture<SendResult<String, String>> = kafkaTemplate.send(topic, message)
+        future.whenComplete { result, throwable ->
+            when (throwable == null) {
+                true -> handleSuccess(result)
+                false -> handleError(result.producerRecord, throwable?.message)
+            }
+        }
+    }
+
+    private fun handleSuccess(result: SendResult<String, String>) {
+        println("Success producing message to topic: ${result.producerRecord}")
+    }
+
+    private fun handleError(
+        producerRecord: ProducerRecord<String, String>,
+        errorMessage: String?
+    ) {
+        println("Error while producing message to topic: $producerRecord, error: $errorMessage")
+    }
+}
